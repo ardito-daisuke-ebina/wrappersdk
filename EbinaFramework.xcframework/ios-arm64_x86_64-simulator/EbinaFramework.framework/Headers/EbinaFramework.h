8c521436dca2b125e6f98c72c1e34a7ede1efb51
daisuke.ebina <daisuke.ebina@ardito.co.jp>
2024-03-12 19:43:59 +0900

    add wrapper

---
+++ EbinaFramework.xcframework/ios-arm64_x86_64-simulator/EbinaFramework.framework/Headers/EbinaFramework.h
@@ -0,0 +1,18 @@
+//
+//  EbinaFramework.h
+//  EbinaFramework
+//
+//  Created by ebina on 2024/03/08.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for EbinaFramework.
+FOUNDATION_EXPORT double EbinaFrameworkVersionNumber;
+
+//! Project version string for EbinaFramework.
+FOUNDATION_EXPORT const unsigned char EbinaFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EbinaFramework/PublicHeader.h>
+
+
---
+++ Package.swift
@@ -0,0 +1,33 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+
+
+import PackageDescription
+let package = Package(
+    name: "EbinaFramework",
+    platforms: [
+        .iOS(.v13)
+    ],
+    products: [
+        .library(
+            name: "EbinaFramework",
+            targets: ["EbinaframeworkWrapper"])    // <--- reference the wrapper here
+    ],
+    dependencies: [
+        .package(url: "https://github.com/Alamofire/Alamofire.git", .upToNextMajor(from: "5.0.0"))
+    ],
+    targets: [
+        .target(
+            name: "EbinaframeworkWrapper",         // <--- new wrapper
+            dependencies: [
+                .product(name: "Alamofire", package: "Alamofire"),
+                .target(name: "EbinaFramework")    // <-- reference the actual binary target here
+            ],
+            path: "Sources/wrappersdk",
+            publicHeadersPath: ""
+        ),
+        .binaryTarget(
+            name: "EbinaFramework",
+            path: "EbinaFramework.xcframework")
+    ])
